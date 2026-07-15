09efad4c5cc66edaeec54a67f49a56d92dca69b0
Faticc <komeristyj07@gmail.com>
2026-07-15 22:52:43 +0300

    0.1.1 structural refactor

--- auth/__init__.py
+++ auth/__init__.py
@@ -1,48 +1,18 @@
-import grpc
-import auth.auth_pb2 as auth_pb2
-import auth.auth_pb2_grpc as auth_pb2_grpc
+# auth/__init__.py
+from .core import send_request
+from .models import (
+    Skin,
+    Cloak,
+    PlayerProfile,
+    ClientProfile,
+    Params
+)
 
-SERVER = "launchernew.mcskill.ru:443"
-
-
-class Profile:
-    def __init__(self, uuid: str, username: str, skin_url: str):
-        self.uuid = uuid
-        self.username = username
-        self.skin_url = skin_url
-
-
-def send_request(username: str, password: str):
-    channel = grpc.secure_channel(SERVER, grpc.ssl_channel_credentials())
-    stub = auth_pb2_grpc.AuthServiceStub(channel)
-
-    request = auth_pb2.LoginRequest(
-        username=username,
-        password=password
-    )
-
-    try:
-        response = stub.Login(request)
-
-        # Успешная авторизация
-        if response.HasField("session_data"):
-            session = response.session_data
-            profile = Profile(
-                uuid=session.profile.uuid,
-                username=session.profile.username,
-                skin_url=session.profile.skin_url
-            )
-            return profile, session.id
-
-        # MFA
-        if response.HasField("mfa_required"):
-            return {"error": "MFA required", "code": "MFA"}
-
-        return {"error": "Unknown response", "code": "UNKNOWN"}
-
-    except grpc.RpcError as e:
-        
-        return {
-            "error": e.details(),
-            "code": e.code().name
-        }
+__all__ = [
+    "Skin",
+    "Cloak",
+    "PlayerProfile",
+    "ClientProfile",
+    "Params",
+    "send_request",
+]
